36a21e2bcc7ab8169b759f76a608350d45920690
weiqitoby600 <weiqitoby600@users.noreply.github.com>
2014-09-06 15:37:22 -0400

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,36 @@
+package learning_notes;
+
+public class BubbleSort {
+	public static void bubbleSort(int[] A) {
+		if (A == null || A.length < 2)
+			return;
+
+		boolean doMore = true;
+		while (doMore) {
+			doMore = false;
+
+			for (int i = 0; i < A.length - 1; i++) {
+				if (A[i] > A[i + 1]) {
+					swap(A, i, i + 1);
+					doMore = true;
+				}
+			}
+		}
+	}
+
+	public static void swap(int[] A, int i, int j) {
+		int temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+	}
+
+	public static void main(String[] args) {
+		int[] A = new int[] { 5, 6, 1, 4, 3, 7, 9, 10 };
+
+		// bubbleSort(A);
+		BubbleSort b = new BubbleSort();
+
+		for (int i : A)
+			System.out.println(i + " ");
+	}
+}
